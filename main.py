d9569b248df796c8c093046fca8fec619a3da8fe
Fazik0105 <72010088+Fazik0105@users.noreply.github.com>
2022-06-24 02:01:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1948,6 +1948,8 @@ def bot_message(message):
             bot.send_audio(message.chat.id,r'https://t.me/TLUSER_4515/1155?single')
             bot.send_audio(message.chat.id,r'https://t.me/TLUSER_4515/1156?single')
             bot.send_audio(message.chat.id,r'https://t.me/TLUSER_4515/1157?single')
+       else:
+            bot.send_message(message.chat.id,'It is wrong message')
         
 
        
